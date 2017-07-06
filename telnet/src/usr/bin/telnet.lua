5b601edb2ca4b59c752198dfff8ba8317edd6c01
LeshaInc <includeurl@gmail.com>
2017-07-06 20:45:14 +0300

    Improve help

--- telnet/src/usr/bin/telnet.lua
+++ telnet/src/usr/bin/telnet.lua
@@ -1,36 +1,58 @@
 local shell = require("shell")
 
-local PROMPT = "$mtelnet> "
-local escapeOn
+local PROMPT = "$(36)telnet> "
+local escapeOn = true
 
-local colors = {
-  ["0"] = 30, r = 31, g = 32, y = 33, b = 34, m = 35, c = 36, w = 37
-}
+local args, opts = shell.parse(...)
 
-local function printfc(fmt, ...)
-  io.write(({fmt:format(...):gsub("$(.)(b?)", function(color, bg)
-    return color == "$" and "$" or (colors[color] and "\27[" .. (bg == "b" and colors[color] + 10 or colors[color]) .. "m" or "$" .. color .. bg)
-  end)})[1] .. "\27[0m")
+-- 30 black, 31 red, 32 green, 33 yellow, 34 blue, 35 magenta, 36 cyan, 37 white
+local writefc
+
+opts.color = opts.color or "auto"
+if opts.color == "auto" then
+  opts.color = io.stdout.tty and "always" or "never"
 end
 
-local function printHelp()
-  print([=[Usage: telnet [OPTION]... [HOST [PORT]]
-  HOST            specify a host to contact over the network
-  PORT            specify a port number to contact (default: 23)
-  -E              disable the escape key functionality
-  --escape=key    WIP: set the escape key (default: C-])
-  -h, --help      display this help and exit]=])
+if opts.color == "always" then
+  function writefc(fmt, ...)
+    io.write(({fmt:format(...):gsub("$%(([^)]+)%)", function(c)
+      return c == "$" and "$" or "\27[" .. c .. "m"
+    end)})[1] .. "\27[0m")
+  end
+elseif opts.color == "never" then
+  function writefc(fmt, ...)
+    io.write(({fmt:format(...):gsub("$%(([^)]+)%)", function(c)
+      return c == "$" and "$" or ""
+    end)})[1])
+  end
+else
+  io.stderr:write("Invalid value for --color=WHEN option; WHEN should be auto, always or never\n")
+  return 2
 end
+
+
+if opts.help then
+  writefc([=[Usage: telnet [OPTION]... [HOST [PORT]]$(0)
+  HOST                  specify a host to contact over the network
+  PORT                  specify a port number to contact (default: 23)
+      --color=WHEN      WHEN can be
+                        auto - colorize output only if writing to a tty,
+                        always - always colorize output,
+                        never - never colorize output; (default: auto)
+  -E                    disable the escape key functionality
+      --escape=key      [WIP] set the escape key (default: C-])
+      --help            display this help and exit
+]=])
+  return 0
+end
+
+if opts.E then escapeOn = false end
+
 
 local function main(...)
-  local args, opts = shell.parse(...)
-  if opts.h or opts.help then
-    printHelp()
-    return
-  end
 
   escapeOn = opts.E ~= null and opts.E or true
-  printfc("%s", PROMPT)
+  writefc("%s", PROMPT)
 end
 
 main(...)
